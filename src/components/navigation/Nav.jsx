7a13e8ed1a512ad1256c65adf9058e44c948e802
Anurag <anuragsinghg99@gmail.com>
2022-07-25 16:20:37 +0530

    changes-some-navbar-code

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import svg from "./align-right-solid.svg";
 import "./nav.css";
 const Nav = () => {
   var logo = document.getElementsByClassName("logo");
@@ -10,6 +11,25 @@ const Nav = () => {
     });
   };
 
+  const [colstyle, setcolstyle] = useState({});
+  const [toggle, settoggle] = useState(false);
+
+  const collapse = () => {
+    if (toggle === false) {
+      setcolstyle({
+        right: "0px",
+        transition: "1.5s all",
+      });
+      settoggle(true);
+    } else {
+      setcolstyle({
+        right: "-220px",
+        transition: "1.5s all",
+      });
+      settoggle(false);
+    }
+  };
+
   return (
     <>
       <div className="navbar">
@@ -17,7 +37,24 @@ const Nav = () => {
           Home Maker
         </div>
         <div className="links">
-          <ul>
+          <img className="collapse-btn" src={svg} alt="" onClick={collapse} />
+          <div className="collapse-nav" style={colstyle}>
+            <ul className="phone-ul">
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/">Services</a>
+              </li>
+              <li>
+                <a href="/">Products</a>
+              </li>
+              <li>
+                <a href="/">Services</a>
+              </li>
+            </ul>
+          </div>
+          <ul className="desk-ul">
             <li>
               <a href="/">Home</a>
             </li>
